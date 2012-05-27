6a8b9f04c32bb73ae3b3451b1c32468d6ffe0e13
Ben Hall <Ben@BenHall.me.uk>
2012-05-27 22:33:40 +0100

    Controller tests

---
+++ MvcMusicStore-Completed/MvcMusicStore.Tests.Controller/CassiniExampleTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CassiniDev;
+using System.IO;
+
+namespace MvcMusicStore.Tests.Controller
+{
+    [TestFixture]
+    public class CassiniExampleTests
+    {
+        [Test]
+        public void ServerReturnsAResponse()
+        {
+            CassiniDevServer server = new CassiniDevServer();
+            server.StartServer(Path.Combine(Environment.CurrentDirectory, @"..\..\..\MvcMusicStore"));
+            string url = server.NormalizeUrl("/");
+            Console.WriteLine(url);
+            var client = new EasyHttp.Http.HttpClient();
+            var response = client.Get(url);
+            Console.WriteLine(response.RawText);
+            Assert.IsFalse(string.IsNullOrEmpty(response.RawText));
+        }
+    }
+}
---
+++ MvcMusicStore-Completed/MvcMusicStore.Tests.Controller/ListHomepageTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CassiniDev;
+using System.IO;
+
+namespace MvcMusicStore.Tests.Controller
+{
+    [TestFixture]
+    public class ListHomepageTests
+    {
+        [Test]
+        public void FiveTrackNamesAppearOnHomepage()
+        {
+            CassiniDevServer server = new CassiniDevServer();
+            server.StartServer(Path.Combine(Environment.CurrentDirectory, @"..\..\..\MvcMusicStore"));
+            string url = server.NormalizeUrl("/");
+            var client = new EasyHttp.Http.HttpClient();
+            var response = client.Get(url);
+            var html = response.RawText;
+
+            StringAssert.Contains("The Best Of Men At Work", html);
+            StringAssert.Contains("For Those About To Rock We Salute You", html);
+            StringAssert.Contains("Let There Be Rock", html);
+            StringAssert.Contains("Balls to the Wall", html);
+            StringAssert.Contains("Restless and Wild", html);
+
+            server.StopServer();
+        }
+
+        [Test]
+        public void FiveItemsAppearHomepage()
+        {
+            CassiniDevServer server = new CassiniDevServer();
+            server.StartServer(Path.Combine(Environment.CurrentDirectory, @"..\..\..\MvcMusicStore"));
+            string url = server.NormalizeUrl("/");
+
+            var dom = CsQuery.Server.CreateFromUrl(url);
+            Assert.AreEqual(5, dom.Find("#album-list").Find("li").Length);
+
+            server.StopServer();
+        }
+    }
+}
